810e2b82fe86e81b68daa1f9252637a6ce9dbfa8
Alexandre Junior <assj2004@hotmail.com>
2025-08-28 11:55:13 -0300

    controle de fluxo DIO

---
+++ contole/src/Main.java
@@ -0,0 +1,36 @@
+// Contador.java
+import java.util.Scanner;
+
+class Contador {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Digite o primeiro número: ");
+            int parametroUm = scanner.nextInt();
+
+            System.out.print("Digite o segundo número: ");
+            int parametroDois = scanner.nextInt();
+
+            contar(parametroUm, parametroDois);
+
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+
+        scanner.close();
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        // se o primeiro parâmetro for MAIOR que o segundo, lança exceção
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        int contagem = parametroDois - parametroUm;
+
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
